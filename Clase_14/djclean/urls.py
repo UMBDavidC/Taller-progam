259cce64d1cd50dbfdb5975b224d93aba162b5e2
unknown <Practica@A412-18.unimb.local>
2023-09-14 19:52:52 -0500

    14-09

--- Clase_14/djclean/urls.py
+++ Clase_14/djclean/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import inicio, nosotros
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("inicio/", inicio, name="Home"),
+    path("nosotros/", nosotros, name="Home2"),
 ]
