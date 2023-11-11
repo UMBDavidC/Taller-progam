4629ba82a0655f26e02ed8dcec26af4d4194735a
David Cubillos <dcubillos904@gmail.com>
2023-11-11 15:18:48 -0500

    Arreglos Visuales

--- clase-05/Mi_portafolio/Aplicaciones/ubicaciones/views.py
+++ clase-05/Mi_portafolio/Aplicaciones/ubicaciones/views.py
@@ -11,14 +11,14 @@ def sitios_interes(request):
     #Get my locations
     locations = Lugares.objects.all()
     #Define the initial map
-    initialMap = folium.Map(location=[4.720026,-74.032649], zoom_start=20)
+    initialMap = folium.Map(location=[4.920626,-74.032649], zoom_start=10)
     #Creating the markets
     latitudes = [location.lat for location in locations]
     longitudes = [location.lng for location in locations]
     popups = [location.name for location in locations]
     FastMarkerCluster(data=list(zip(latitudes, longitudes, popups))).add_to(initialMap)
     
-    context = {'map':initialMap._repr_html_()}
-    #context = {'map':initialMap._repr_html_(), 'locations':locations}
+    #context = {'map':initialMap._repr_html_()}
+    context = {'map':initialMap._repr_html_(), 'locations':locations}
     
     return render(request, 'mapa.html',context )
