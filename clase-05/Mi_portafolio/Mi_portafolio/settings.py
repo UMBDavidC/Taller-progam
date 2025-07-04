efd1108133d6c7d915ab94d3df32a0a310fea78a
ArDavid14 <141374647+ArDavid14@users.noreply.github.com>
2025-07-04 15:56:44 -0500

    Update settings.py

--- clase-05/Mi_portafolio/Mi_portafolio/settings.py
+++ clase-05/Mi_portafolio/Mi_portafolio/settings.py
@@ -67,7 +67,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Dcubillos904@gmail.com'
-EMAIL_HOST_PASSWORD = 'saql zhdm cxyt mdop'
+EMAIL_HOST_PASSWORD = '**** **** **** '
 
 
 TEMPLATES = [
